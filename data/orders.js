25b00cf09fd83ee41b8a12a108830377a4a446ed
Grantas Gadliauskas <grantas0612@gmail.com>
2018-02-13 12:33:10 +0200

    second commit

---
+++ data/orders.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 0,
+        title: 'Madman Tri-Series',
+        name: 'John Brietkus',
+        type: 0
+    },
+    {
+        id: 1,
+        title: 'Madman Tri-Series',
+        name: 'Haas Suhlee'
+        type: 0
+    },
+    {
+        id: 2,
+        title: 'Mudslapper',
+        name: 'Max Verstappen'
+        type: 1
+    },
+    {
+        id: 3,
+        title: 'Forester',
+        name: 'Felonzo Betten'
+        type: 1
+    }
+];
