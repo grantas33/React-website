7074c2ba2aa284202ccd28d6e657caaaa9a997a8
Grantas Gadliauskas <grantas0612@gmail.com>
2018-02-13 19:28:57 +0200

    add skeleton

--- src/data/orders.js
+++ src/data/orders.js
@@ -8,19 +8,19 @@ export default [
     {
         id: 1,
         title: 'Madman Tri-Series',
-        name: 'Haas Suhlee'
+        name: 'Haas Suhlee',
         type: 0
     },
     {
         id: 2,
         title: 'Mudslapper',
-        name: 'Max Verstappen'
+        name: 'Max Verstappen',
         type: 1
     },
     {
         id: 3,
         title: 'Forester',
-        name: 'Felonzo Betten'
+        name: 'Felonzo Betten',
         type: 1
     }
 ];
